567258286943c3b2ee5d28688feb0a973dac2d40
iaamvarun <85957398+iaamvarun@users.noreply.github.com>
2021-06-24 17:41:38 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('<h1 style="color:black;">Hi Varun Hello var!!</h1> \n');
+  res.send('<h1 style="color:red;">Hi Varun Hello var!!</h1> \n');
 });
 
 app.listen(PORT, HOST);
